80aa1c51e1378e3277bded5006b314328a926b9e
Frederik Elwert <frederik.elwert@rub.de>
2024-03-09 18:09:43 +0100

    Add comments

--- bookmark_search_app.py
+++ bookmark_search_app.py
@@ -13,18 +13,22 @@ read_bookmarks = st.cache_data(bookmark_search.read_bookmarks)
 
 
 def main():
-    # Parse commandline arguments
+    # Site title
     st.title("Mastodon Bookmark Search")
+    # Get data
     data = read_bookmarks()
+    # Configure sidebar
     with st.sidebar:
         search_text = st.text_input("Search")
         hashtags = bookmark_search.get_hashtags(data)
         tag_options = hashtags.filter(pl.col("count") > 1).get_column("hashtags")
         selected_tags = st.multiselect("Hashtags", options=tag_options)
+    # Filter data
     for tag in selected_tags:
         data = data.filter(pl.col("hashtags").list.contains(tag))
     if search_text:
         data = data.filter(pl.col("text").str.contains(search_text))
+    # Display results
     for row in data.iter_rows(named=True):
         st.markdown(
             f"""
